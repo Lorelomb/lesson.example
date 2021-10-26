35f88b3def4466f3f6eff9d73fe8c3cba9cef04f
2020326 <2020326@brunel.ac.uk>
2021-10-26 13:42:48 +0100

    Second commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+# myList = [9,"tom",[5,6]]
+#
+# newList = myList + [23,24]
+# newList.append("mara")
+# newList.insert(2,800)
+# newList.extend([6,6,6])
+# firstItem = newList[0]
+# print(firstItem)
+# newList[1] = 8
+# print(newList)
+# numberedList = [89,56,99,2]
+# print(sorted(numberedList,reverse=True))
+# if 56 in numberedList:
+#     print("Found in the list")
+
+# inventory =["iron sword","torch","health potion","tentacle sweeper"]
+# question = input("Deadly monster appeared, use tentacle sweeper? y/n: ")
+# item = ("tentacle sweeper")
+# if question == "y":
+#     if item in inventory and item == "tentacle sweeper":
+#         print("The monster was slain")
+# if question == "n":
+#     print("You are dead, no big surprise")
+
+myList = []
